4ad0019bea5faa5814a9f1bd3f50594ee8ce9f96
kaushalshah750 <kaushalshah750@gmail.com>
2025-05-17 18:44:09 +0530

    added all component code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero/hero.component";
+import { FeaturesComponent } from "./components/features/features.component";
+import { PricingComponent } from "./components/pricing/pricing.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HowItWorksComponent } from "./components/how-it-works/how-it-works.component";
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroComponent, FeaturesComponent, PricingComponent, FooterComponent, HowItWorksComponent, TestimonialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  imports: [ReactiveFormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+      alert('Thank you for contacting us!');
+      this.contactForm.reset();
+    }
+  }
+}
